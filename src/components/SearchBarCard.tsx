70a797fd6995f2aff75f2bbc3d256caf29caa10f
cavia <caviagamer.2018@gmail.com>
2025-03-31 14:11:58 +0200

    fix: styling crypto- search bar components improved
    styling improved

--- src/components/SearchBarCard.tsx
+++ src/components/SearchBarCard.tsx
@@ -17,28 +17,29 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch, onCancel }) => {
     };
 
     const handleCancel = () => {
+        setSearchTerm(""); // Clear the input field
         onCancel();
     };
 
     return (
-            <div className="flex items-center gap-2  mb-6">
-                <div className="flex items-center shadow-lg w-1/2">
-                    <input
-                        type="text"
-                        value={searchTerm}
-                        onChange={handleInputChange}
-                        placeholder="Search for a cryptocurrency..."
-                        onFocus={(e) => e.target.classList.add("placeholder-transparent")}
-                        onBlur={(e) => e.target.classList.remove("placeholder-transparent")}
-                        className="p-2 bg-[#282740] w-full focus:outline-none"
-                    />
-                    <button
-                        onClick={handleCancel}
-                        className="bg-[#282740] text-zinc-300 px-4 py-2 hover:bg-[#362339]"
-                    >
-                        Cancel
-                    </button>
-                </div>
+        <div className="flex items-center gap-2 mb-6">
+            <div className="flex items-center shadow-lg w-1/2">
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                    placeholder="Search for a cryptocurrency..."
+                    onFocus={(e) => e.target.classList.add("placeholder-transparent")}
+                    onBlur={(e) => e.target.classList.remove("placeholder-transparent")}
+                    className="p-2 bg-[#282740] w-full focus:outline-none text-zinc-300"
+                />
+                <button
+                    onClick={handleCancel}
+                    className="bg-[#282740] text-zinc-300 px-4 py-2 hover:bg-[#362339]"
+                >
+                    Cancel
+                </button>
+            </div>
             <button
                 onClick={handleSearch}
                 className="bg-[#100d53] text-zinc-300 px-4 py-2 shadow-md transition hover:bg-[#370d53]"
